44e15fa176ccecae952f5516f4ecf647890cb09d
SargeanTravis <69531538+SargeanTravis@users.noreply.github.com>
2020-12-30 19:21:25 -0500

    serialize

---
+++ src/js/game/base_fluid.js
@@ -0,0 +1,23 @@
+import { globalConfig } from "../core/config";
+import { DrawParameters } from "../core/draw_parameters";
+import { BasicSerializableObject } from "../savegame/serialization";
+
+export const fluidTypes =["water"];
+
+export class BaseFluidComponent extends BasicSerializableObject{
+
+    constructor(){
+        super();
+    }
+
+    static getId()
+    {
+        return "base_fluid";
+    }
+
+    static getSchema() {
+        return {};
+    }
+
+
+}
---
+++ src/js/game/systems/pipe_network.js
@@ -0,0 +1,9 @@
+//Will add in relevant Import classes later when development starts
+
+//IMPORTANT: If anyone wishes to use or modify sHape2O, this file MUST be present.
+//Failure to do so will most likely lead to game crashes, as all of the additions
+// rely on methods within here.
+
+
+
+
